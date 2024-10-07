b5e20d5d6fca89114e5cbfccb8ae04a8fe69b1e1
Grave <guilermecarvalho@live.com>
2024-10-07 19:58:35 -0300

    exercicio_grunt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('form-sorteador').addEventListener('submit', function(e) {
-        e.preventDefault();
-        let numeroMaximo = document.getElementById('numero-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
+const form = document.getElementById('form');
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
-        document.querySelector('.resultado').style.display = 'block';
-    })
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+
+    const imc = (peso / (altura * altura)).toFixed(2);
+
+    const valor = document.getElementById('valor');
+    let descricao = "";
+
+    document.getElementById('infos').classList.remove('hidden');
+
+    if (imc < 18.5) {
+        descricao = 'Cuidado! Você está abaixo do peso'
+    } else if ( imc >= 18.5 && imc <= 25) {
+        descricao = "Você está no peso ideal!";
+    } else if ( imc > 25 && imc <= 30) {
+        descricao = "Cuidado! Você está com sobrepeso!";
+    } else if ( imc >= 30 && imc <= 35) {
+        descricao = "Cuidado! Você está com obesidade moderada!";
+    } else {
+        descricao = "Cuidado! Você está com obesidade morbida!";
+    }
+
+    valor.textContent = imc.replace('.', ',');
+    document.getElementById('descricao').textContent = descricao;
 })
